f1c3c34775bbb3cc01d5a96dac8c7ded0e65ac17
Sachin Yadav <147396495+sachinyadav20187@users.noreply.github.com>
2024-01-31 13:16:17 +0530

    Controller for vhire created with command line

---
+++ activiti-poc/src/main/java/com/activitipoc/controllers/VhireController.java
@@ -0,0 +1,36 @@
+package com.activitipoc.controllers;
+
+
+import com.activitipoc.entities.Applicant;
+import com.activitipoc.repositories.ApplicantRepository;
+import org.activiti.engine.RuntimeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+import java.util.Map;
+
+@RestController
+public class VhireController {
+
+    @Autowired
+    private RuntimeService runtimeService;
+
+    @Autowired
+    private ApplicantRepository applicantRepository;
+
+    @ResponseStatus(value = HttpStatus.OK)
+    @RequestMapping(value = "/start-vhire", method = RequestMethod.POST,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public void startHireProcess(@RequestBody Map<String, String> data) {
+
+        Applicant applicant = new Applicant(data.get("applicantName"), data.get("emailId"), data.get("phoneNumber"));
+        applicantRepository.save(applicant);
+
+        Map<String, Object> vars = Collections.<String, Object>singletonMap("applicant", applicant);
+        runtimeService.startProcessInstanceByKey("vhire", vars);
+    }
+
+}
