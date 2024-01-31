4ff42c293b9b68a73f857223539c0292ee723978
Akash nag <88840727+buffer-reader@users.noreply.github.com>
2024-01-31 11:43:01 +0530

    made structure

---
+++ activiti-poc/src/main/java/com/activitipoc/ActivitiPocApplication.java
@@ -0,0 +1,13 @@
+package com.activitipoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivitiPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivitiPocApplication.class, args);
+	}
+
+}
---
+++ activiti-poc/src/test/java/com/activitipoc/ActivitiPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.activitipoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivitiPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
